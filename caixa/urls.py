4601fc095c952cc53ea41b7346bd6c0a51d9f642
redcliver <igor-peres@hotmail.com>
2017-08-08 18:54:07 -0300

    1.0

--- caixa/urls.py
+++ caixa/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.caixa1),
     url(r'^extrato/', views.extrato),
+    url(r'^retirada/', views.retirada),
+    url(r'^fechamento/', views.fechamento),
     ]
--- caixa/views.py
+++ caixa/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import caixa
 import datetime
+from decimal import *
 # Create your views here.
 
 def caixa1(request):
@@ -11,3 +12,29 @@ def extrato(request):
     hoje = datetime.date.today()
     caixas = caixa.objects.filter(data__contains=hoje)
     return render(request, 'extrato.html', {'title':'Extrato', 'caixas':caixas})
+
+def retirada(request):
+    total = caixa.objects.latest('id')
+    if request.method == 'POST':
+        valor = request.POST.get('valor')
+        motivo = request.POST.get('obs')
+        total1 = total.total
+        total2 = total1 - Decimal(valor)
+        retirada1 = caixa(total=total2, desc=motivo, tipo='S')
+        retirada1.save()
+        msg = "Retirada efetuada com sucesso!"
+        return render(request, 'home/home.html', {'title':'Home', 'msg':msg})
+    return render(request, 'retirada.html', {'title':'Retirada', 'total':total})
+
+def fechamento(request):
+    total = caixa.objects.latest('id')
+    if request.method == 'POST':
+        valor = request.POST.get('valor')
+        motivo = "Fechamento"
+        total1 = total.total
+        total2 = total1 - Decimal(valor)
+        retirada1 = caixa(total=total2, desc=motivo, tipo='S')
+        retirada1.save()
+        msg = "Caixa fechado com sucesso!"
+        return render(request, 'home/home.html', {'title':'Home', 'msg':msg})
+    return render(request, 'fechamento.html', {'title':'Fechamento', 'total':total})
