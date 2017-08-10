5491a4c54defa71cf3fa226cb63bf11b0b53868c
redcliver <igor-peres@hotmail.com>
2017-08-10 10:02:54 -0300

    1.0

--- outros/urls.py
+++ outros/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     url(r'^$', views.outros),
     url(r'^addproduto/', views.addproduto),
     url(r'^addservico/', views.addservico),
-    url(r'^addservico1/', views.addservico1),
     url(r'^addfuncionario/', views.addfuncionario),
     url(r'^buscacomanda/', views.buscacomanda),
     url(r'^buscacomandacorte/', views.buscacomandacorte),
