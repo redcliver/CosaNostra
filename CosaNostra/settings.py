1b071c56427a214e257f3336c1a84d3b69e166f4
redcliver <igor-peres@hotmail.com>
2017-08-14 17:35:29 -0400

    Update settings.py

--- CosaNostra/settings.py
+++ CosaNostra/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'CosaNostra.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'IGOR',                      
         'USER': 'IGOR',
         'PASSWORD': 'igor3355',
